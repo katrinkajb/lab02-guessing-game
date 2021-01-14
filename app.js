dc4a991102d69c6386ba9bfb3d8962b5eecb9d38
katrinkajb@gmail.com <katrinkajb@gmail.com>
2021-01-14 09:11:28 -0800

    fixed bugs and started work on reset button

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const numberInput = document.getElementById("number-input");
 const randomNumber = randomNumberFunc();
 const feedbackArea = document.getElementById("user-feedback-area");
 const guessesRemainingSpan = document.getElementById("guesses-remaining-span");
+const resetButton = document.getElementById('reset-button');
+
 
 let guessesRemaining = 4;
     console.log(randomNumber);
@@ -14,15 +16,24 @@ guessButton.addEventListener('click', () =>{
     guessesRemaining--;
     
     if (Number(numberInput.value) === randomNumber) {
-        guessButton.disable = 'true';
+        guessButton.disabled = 'true';
         feedbackArea.textContent = 'Great job!';
     }
     else if (guessesRemaining <= 0) {
-        guessButton.disable = 'true';
+        guessButton.disabled = 'true';
         feedbackArea.textContent = 'Sorry, you lose';
+        guessesRemainingSpan.textContent = '';
     }
     else {
         feedbackArea.textContent = guessComparison(numberInput.value, randomNumber);
         guessesRemainingSpan.textContent = guessesRemaining + ' guesses remaining';
     }
 })
+
+// resetButton.addEventListener('click', () => {
+//     guessesRemaining = 4;
+//     randomNumberFunc();
+//     guessesRemainingSpan.textContent = guessesRemaining + ' guesses';
+//     guessButton.disabled = 'false';
+//     numberInput.textContent = '';
+// })
